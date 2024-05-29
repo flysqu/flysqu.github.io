75ce5923661631f3905b77352b9e1279ee08a374
Flysqu <flysqu@proton.me>
2024-05-29 19:48:02 +0200

    uhhh did some stuff

---
+++ nginx/html/script.js
@@ -0,0 +1,6 @@
+import { rainbowCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+    new rainbowCursor({
+        length: 12,
+        colors: ["#5BCEFA","#F5A9B8","#FFFFFF","#F5A9B8","#5BCEFA"],
+        size: 4,
+});

67b2adc5d7b8031a85641fcb748702e89aa35105
Flysqu <sabpatty97@gmail.com>
2024-08-10 16:30:43 +0300

    Remove reinbowCursor completely uwu
    :3

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-import { rainbowCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
-new rainbowCursor({
-    length: 12,
-    colors: ["#5BCEFA", "#F5A9B8", "#FFFFFF", "#F5A9B8", "#5BCEFA"],
-    size: 4,
-});
-
 let highestZIndex = 0
 
 // fucky fix

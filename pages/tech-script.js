c4f2330a66382521955a9e3f158957a53cca8149
Flysqu <98214547+flysqu@users.noreply.github.com>
2025-05-06 20:03:45 +0200

    typo

--- pages/tech-script.js
+++ pages/tech-script.js
@@ -266,7 +266,7 @@ export function techSpawnGif(path,isLaptopPic) {
 function macbook()  { techSpawnGif("../resources/pictures/macbook.JPG", true)  }
 function surface()  { techSpawnGif("../resources/pictures/surface.JPG", true)  }
 function thinkpad() { techSpawnGif("../resources/pictures/thinkpad.JPG", true) }
-function pixel3a()  { techSpawnGif("../resources/pictrues/pixel3a.JPG", true)}
+function pixel3a()  { techSpawnGif("../resources/pictures/pixel3a.JPG", true)}
 
 document.getElementById("macbook").addEventListener("click",macbook);
 document.getElementById("surface").addEventListener("click",surface);
